660b50a1bd7f55873dd6bf250aec1f542e004313
mackenziestein <mackenziestein415@gmail.com>
2016-12-04 12:24:35 -0500

    added registers to registerFile - now have 8

--- registerFile.sv
+++ registerFile.sv
@@ -17,9 +17,22 @@ module registerFile(input logic [4:0] A1,
    
    assign yesWrite0 = WE3 &  ~A3[2] & ~A3[1] & ~A3[0];
    assign yesWrite1 = WE3 &  ~A3[2] & ~A3[1] & A3[0];
+   assign yesWrite2 = WE3 &  ~A3[2] & A3[1] & ~A3[0];
+   assign yesWrite3 = WE3 &  ~A3[2] & A3[1] & A3[0];
+   assign yesWrite4 = WE3 &  A3[2] & ~A3[1] & ~A3[0];
+   assign yesWrite5 = WE3 &  A3[2] & ~A3[1] & A3[0];
+   assign yesWrite6 = WE3 &  A3[2] & A3[1] & ~A3[0];
+   assign yesWrite7 = WE3 &  A3[2] & A3[1] & A3[0];
      
    enabledRegister r0(WD3,reg0,CLK,yesWrite0);
    enabledRegister r1(WD3,reg1,CLK,yesWrite1);
+   enabledRegister r2(WD3,reg1,CLK,yesWrite2);
+   enabledRegister r3(WD3,reg1,CLK,yesWrite3);
+   enabledRegister r4(WD3,reg1,CLK,yesWrite4);
+   enabledRegister r5(WD3,reg1,CLK,yesWrite5);
+   enabledRegister r6(WD3,reg1,CLK,yesWrite6);
+   enabledRegister r7(WD3,reg1,CLK,yesWrite7);
+ 
       
    mux4to1B32 mpxA1(A1[1],A1[0],32'b101010,32'b111111,reg1, reg0, RD1);
 
@@ -30,6 +43,12 @@ module registerFile(input logic [4:0] A1,
      begin
 	$display("register 0 %h ",reg0);
 	$display("register 1 %h ",reg1);
+	$display("register 2 %h ",reg2);
+	$display("register 3 %h ",reg3);
+	$display("register 4 %h ",reg4);
+	$display("register 5 %h ",reg5);
+	$display("register 6 %h ",reg6);
+	$display("register 7 %h ",reg7);
 	
      end
    
