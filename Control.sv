131c7dc4ea2482ea3adb4292dbb60f3f319d0f1c
mackenziestein <mackenziestein415@gmail.com>
2016-12-08 21:21:51 -0500

    fixed control

--- Control.sv
+++ Control.sv
@@ -1,7 +1,7 @@
 module Control(ins, memToReg, memWrite, branchEnable, ALUControl, ALUSrc, regDst, regWriteEnable, jump, jumpReg, alu4, alu3, alu2, alu1, alu0);
 
    input logic [31:0] ins;
-   output logic [0:0] memToReg, memWrite, branchEnable, ALUSrc, regDst, regWriteEnable, jump, jumpReg; alu4, alu3, alu2, alu1, alu0; 
+   output logic [0:0] memToReg, memWrite, branchEnable, ALUSrc, regDst, regWriteEnable, jump, jumpReg, alu4, alu3, alu2, alu1, alu0; 
    output logic [4:0] ALUControl;
    
 
